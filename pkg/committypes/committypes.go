9ff869d0db22fdcd2be41ed1589513aa7f6ba7c9
Renato Guilhermini <renato.guilhermini@luizalabs.com>
2025-02-02 12:29:21 -0300

    🐛 fix: Emoji not appearing when using --emoji

--- pkg/committypes/committypes.go
+++ pkg/committypes/committypes.go
@@ -10,9 +10,17 @@ var validTypes = []string{
 	"feat", "fix", "docs", "style", "refactor", "test", "chore", "perf", "build", "ci",
 }
 
-// typeWithEmojiPattern precompiles the regex used to match
-// an optional emoji, a valid commit type, an optional scope, and a colon.
-var typeWithEmojiPattern = regexp.MustCompile(`^((\p{So}|\p{Sk}|:\w+:)\s*)?(` + strings.Join(validTypes, "|") + `)(\([^)]+\))?:`)
+// BuildRegexPatternWithEmoji returns a precompiled regex that matches
+// the Conventional Commit type with an optional emoji and scope.
+func BuildRegexPatternWithEmoji() *regexp.Regexp {
+	// Build a regex that matches:
+	// - an optional emoji or gitmoji in the form of a unicode symbol or :emoji:
+	// - followed by one of the valid commit types (e.g., feat, fix, etc.)
+	// - optionally a scope in parentheses
+	// - and a colon at the end.
+	pattern := `^((\p{So}|\p{Sk}|:\w+:)\s*)?(` + strings.Join(validTypes, "|") + `)(\([^)]+\))?:`
+	return regexp.MustCompile(pattern)
+}
 
 // IsValidCommitType checks if the provided commit type is in the list of valid types.
 func IsValidCommitType(t string) bool {
@@ -32,10 +40,4 @@ func AllTypes() []string {
 // TypesRegexPattern returns a string that can be used to match valid commit types in a regex.
 func TypesRegexPattern() string {
 	return strings.Join(validTypes, "|")
-}
-
-// BuildRegexPatternWithEmoji returns a precompiled regex that matches
-// the Conventional Commit type with an optional emoji and scope.
-func BuildRegexPatternWithEmoji() *regexp.Regexp {
-	return typeWithEmojiPattern
 }
